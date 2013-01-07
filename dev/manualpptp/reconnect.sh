0764f5d1d907757b0822825dc94b39c1ba39bf0a
i@blue.cat <i@blue.cat@f2cc9dee-0699-b7a6-1b3b-f08308815720>
2013-01-07 03:07:34 +0000

    add sth

--- dev/manualpptp/reconnect.sh
+++ dev/manualpptp/reconnect.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+VPNLOG='/tmp/autoddvpn.log'
+PID=$$
+INFO="[INFO#${PID}]"
+
 sleep 2
+echo "$INFO $(date "+%d/%b/%Y:%H:%M:%S") trying to kill PPTP process" >> $VPNLOG
 PPTPPID=`pidof -s pptp`
 kill $PPTPPID
 sleep 2
 kill -9 $PPTPPID
 sleep 2
-pptp <vpn_server_ip> file /jffs/pptp/manual/options.vpn &
+echo "$INFO $(date "+%d/%b/%Y:%H:%M:%S") trying to reconnect to PPTP server" >> $VPNLOG
+pptp $(cat /jffs/pptp/manual/ipaddress.conf) file /jffs/pptp/manual/options.vpn &
 exit 0
