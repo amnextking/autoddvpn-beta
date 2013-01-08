ebbe27463df781316bd5c209687086bd9faa2f6b
i@blue.cat <i@blue.cat@f2cc9dee-0699-b7a6-1b3b-f08308815720>
2013-01-08 01:11:37 +0000

    some little changes

--- dev/manualpptp/firstconnect.sh
+++ dev/manualpptp/firstconnect.sh
@@ -20,20 +20,19 @@ fi
 
 # Set nvram
 echo "$INFO $(date "+%d/%b/%Y:%H:%M:%S") vpnsrvsub.conf and vpnsrvsubmsk.conf found, setting nvram..." >> $VPNLOG
-nvram set pptpd_client_srvsub="$(cat /jffs/pptp/manual/vpnsrvsub.conf)"
-nvram set pptpd_client_srvsubmsk="$(cat /jffs/pptp/manual/vpnsrvsubmsk.conf)"
+nvram set pptpd_client_srvsub="`cat /jffs/pptp/manual/vpnsrvsub.conf`"
+nvram set pptpd_client_srvsubmsk="`cat /jffs/pptp/manual/vpnsrvsubmsk.conf`"
 nvram commit
 
 # Resolve VPN IP address
 if [ -f /jffs/pptp/manual/domainname.conf ]; then
     rm -f /jffs/pptp/manual/ipaddress.conf
     echo "$INFO $(date "+%d/%b/%Y:%H:%M:%S") trying to resolve VPN IP address from domain name" >> $VPNLOG
-    echo $(ping -w 1 -c 1 $(cat /jffs/pptp/manual/domainname.conf) | grep -Eo "\(([0-9.]+)" | cut -d\( -f 2) >> /jffs/pptp/manual/ipaddress.conf
-    if [ $(cat /jffs/pptp/manual/ipaddress.conf) == $(ping -w 1 -c 1 domainnotexists | grep -Eo "\(([0-9.]+)" | cut -d\( -f 2) ]; then
-        rm -f /jffs/pptp/manual/ipaddress.conf
+    if [ $(ping -w 1 -c 1 `cat /jffs/pptp/manual/domainname.conf` | grep -Eo "\(([0-9.]+)" | cut -d\( -f 2) == $(ping -w 1 -c 1 domainnotexists | grep -Eo "\(([0-9.]+)" | cut -d\( -f 2) ]; then
         echo "$ERROR $(date "+%d/%b/%Y:%H:%M:%S") Can't resolve the ip address from domainname.conf" >> $VPNLOG
         exit 1
     else
+        echo -n $(ping -w 1 -c 1 `cat /jffs/pptp/manual/domainname.conf` | grep -Eo "\(([0-9.]+)" | cut -d\( -f 2) >> /jffs/pptp/manual/ipaddress.conf
         echo "$INFO $(date "+%d/%b/%Y:%H:%M:%S") resolved the ip address from domainname.conf and saved it to ipaddress.conf" >> $VPNLOG
     fi
 fi
@@ -44,5 +43,5 @@ fi
 
 #Connect to VPN
 echo "$INFO $(date "+%d/%b/%Y:%H:%M:%S") ALL DONE. Let's wait for VPN being connected." >> $VPNLOG
-pptp $(cat /jffs/pptp/manual/ipaddress.conf) file /jffs/pptp/manual/options.vpn &
+pptp `cat /jffs/pptp/manual/ipaddress.conf` file /jffs/pptp/manual/options.vpn &
 exit 0
