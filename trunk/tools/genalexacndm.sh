c94723c78343713160a8c6d0e525d5bcedfdd5ee
i@blue.cat <i@blue.cat@f2cc9dee-0699-b7a6-1b3b-f08308815720>
2013-01-13 06:19:05 +0000

    new upload

---
+++ trunk/tools/genalexacndm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ispdns='202.96.209.5'
+for i in 0 1 2 3 4
+do
+    curl -s "http://www.alexa.com/topsites/countries;$i/CN" | grep "small topsites-label"  | \
+    sed -e "s#.*>\([^ ]*\)<.*#server=/\1/$ispdns#g"
+done
+
