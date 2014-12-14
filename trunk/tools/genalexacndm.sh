8400ade1ca6103c35a2935bdcde71fb2126a959c
i@blue.cat <i@blue.cat@f2cc9dee-0699-b7a6-1b3b-f08308815720>
2014-12-14 11:57:14 +0000

    add script description & comments

--- trunk/tools/genalexacndm.sh
+++ trunk/tools/genalexacndm.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+# This is the tool generating the DNSMasq configuration file optimizing the visit for websites inside China according to Alexa Top Sites
 # from http://code.google.com/p/autoddvpn/wiki/DNSMasq
 
+# change this to your Chinese ISP DNS server address
 ispdns='202.96.209.5'
+
 for i in 0 1 2 3 4
 do
     curl -s "http://www.alexa.com/topsites/countries;$i/CN" | grep "small topsites-label"  | \
     sed -e "s#.*>\([^ ]*\)<.*#server=/\1/$ispdns#g"
 done
-
